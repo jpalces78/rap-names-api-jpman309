a1e5100da8d824190f593304aca2b1f9d0ca002c
jpalces78 <jpman309@gmail.com>
2022-06-01 19:56:41 -0500

    server files

---
+++ rap-names-api/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage':{
+        'age': 29,
+        'birthName': `Bri'ish` ,
+        'birthLocation':'London, UK'
+    },
+    'chance the rapper':{
+        'age': 29,
+        'birthName': `Chancellor Bennett` ,
+        'birthLocation':'Chicago, IL'
+    },
+    'dylan':{
+        'age': 29,
+        'birthName': `Dylan` ,
+        'birthLocation':'Dylan, Dylan'
+    }
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (req,res)=> {//colon signifies a query param
+    const rapperName = req.params.rapperName.toLowerCase()
+    if(rappers[rapperName]){
+        res.json(rappers[rapperName])
+    }else{
+        res.json(rappers['dylan'])
+    }
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is running on port ${PORT}`)
+})
